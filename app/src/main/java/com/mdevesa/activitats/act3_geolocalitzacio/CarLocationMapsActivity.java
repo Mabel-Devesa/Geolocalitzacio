d9b87b8bc4cacf030888582c1704b47f741c9965
Mabel-Devesa <MDevesa17@jaumebalmes.net>
2019-02-06 20:44:37 +0100

    3r Commit

--- app/src/main/java/com/mdevesa/activitats/act3_geolocalitzacio/CarLocationMapsActivity.java
+++ app/src/main/java/com/mdevesa/activitats/act3_geolocalitzacio/CarLocationMapsActivity.java
@@ -1,18 +1,34 @@
 package com.mdevesa.activitats.act3_geolocalitzacio;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CarLocationMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CarLocationMapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private Button boto;
+    private LatLng posicioActual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +39,48 @@ public class CarLocationMapsActivity extends FragmentActivity implements OnMapRe
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Activar el sistema de localitzacio
+        LocationManager gestorLoc = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        gestorLoc.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+
+
         /*Objectius:
           -Un boto que ens crei una marca en la ubicacio actual
           -Cal que el titol de la marca sigui el nom de la poblacio
-          -Cal indicar una descripcio concorde amb el servei
+          -Esborrar el anterior marcador quan es crea el nou
         **/
+        boto = (Button) findViewById(R.id.button);
+
+        boto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Es crea un marcador amb la seva informacio
+                MarkerOptions opcions = new MarkerOptions();
+                opcions.title("El teu cotxe esta aqui");
+                opcions.position(posicioActual);
+                opcions.snippet("El teu cotxe esta aparcat aqui");
+                opcions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+
+                //Un cop creat el marquem al mapa
+                Marker marca = mMap.addMarker(opcions);
+
+                //Serveix per mostrar la seva informacio sense haver de tocar-la
+                marca.showInfoWindow();
+
+                //https://stackoverflow.com/questions/13692398/remove-a-marker-from-a-googlemap
+                //esborrar el marcador antic
+            }
+        });
 
     }
 
@@ -49,5 +102,50 @@ public class CarLocationMapsActivity extends FragmentActivity implements OnMapRe
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //Crear les coordenades de la posicio actual
+        LatLng posicio = new LatLng(location.getLatitude(),location.getLongitude());
+        //Afegir la camera amb el punt generat abans i un nivell de zoom
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(posicio,5);
+        //Desplacem la camera al nou punt
+        mMap.moveCamera(camera);
+        posicioActual = posicio;
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        String missatge = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                missatge = "GPS status: Out of service";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                missatge = "GPS status: Temporarily unavailable";
+                break;
+            case LocationProvider.AVAILABLE:
+                missatge = "GPS status: Available";
+                break;
+        }
+        Toast.makeText(getApplicationContext(),
+                missatge,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(getApplicationContext(),
+                "GPS habilitat per l'usuari",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(getApplicationContext(),
+                "GPS desactivat per l'usuari",
+                Toast.LENGTH_LONG ).show();
     }
 }
